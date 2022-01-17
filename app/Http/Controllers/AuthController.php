74c641d79c738d80639facc1b263945966476fc9
Kingsley Udenewu <kingzpacking@gmail.com>
2022-01-17 02:30:32 +0100

    Add .env.testing file for unit and feature testing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Services\AuthService;
-use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+use Illuminate\Support\Arr;
 
 class AuthController extends Controller
 {
+    protected $authService;
+
     public function __construct()
     {
-        $authService = resolve(AuthService::class);
+        $this->authService = resolve(AuthService::class);
     }
 
     public function register(RegisterRequest $request)
     {
+        $userSignUp = $this->authService->registerUser($request->toArray());
+        if (Arr::get($userSignUp, 'status')) {
+            return $this->createdResponse('Successful, welcome to Aspire Loan',
+                Arr::get($userSignUp, 'data'));
+        }
+    }
 
+    public function login(LoginRequest $request)
+    {
+        $payload = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        $userLogin = $this->authService->loginUser($payload);
+        if (Arr::get($userLogin, 'status')) {
+            return $this->successResponse('Login successful to Aspire Loan',
+                Arr::get($userLogin, 'data'));
+        }
     }
 }
