71e0bf18efccb6ed91d3473a3ddd0b160a12f2d2
Kingsley Udenewu <kingzpacking@gmail.com>
2022-01-17 11:29:48 +0100

    refactored authservice

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,21 +3,47 @@
 namespace App\Services;
 
 use App\Models\User;
+use App\Traits\ApiResponse;
 use Carbon\Carbon;
 use Illuminate\Support\Arr;
 
 class AuthService
 {
-    public function register(array $attributes)
+    use ApiResponse;
+
+    /**
+     * @param array $attributes
+     */
+    public function registerUser(array $attributes)
     {
         $attributes['password'] = bcrypt(Arr::get($attributes, 'password'));
         $user = User::create($attributes);
-        $tokenResult = $user->createToken('Personal Access Token');
+        $payload = $this->createToken($user);
+        return $this->ok($payload);
+    }
+
+    public function loginUser(array $attributes)
+    {
+        if (!auth()->attempt($attributes)) {
+            abort(400, 'Invalid login credentials inputted');
+        }
+        $payload = $this->createToken(auth()->user());
+        return $this->ok($payload);
+    }
+
+    /**
+     * Format passport token to be generated to a user
+     * @param User $user
+     * @return array
+     */
+    private function createToken(User $user): array
+    {
+        $tokenResult = $user->createToken('Personal Access Token For Aspire Loan');
         $token = $tokenResult->token;
 
-        $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->expires_at = Carbon::now()->addHours(1);
         $token->save();
-        $payload =  [
+        return  [
             'token' => $tokenResult->accessToken,
             'user' => $user,
             'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
